50c153abc6bcb32629c06d4cce0ccd0edc3caf66
CHARUMATHIBALA <155135192+CHARUMATHIBALA@users.noreply.github.com>
2025-12-19 09:29:12 +0530

    2427

---
+++ 2427.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int min = 0;
+        int count = 0;
+        if ( a > b)
+        {
+            min = b ;
+        }
+        else
+        {
+            min = a;
+        }
+        for(int i = 1 ;i<=min ;i++)
+        {
+            if(a % i == 0 && b % i == 0)
+            {
+                 count = count + 1;
+            }
+        }
+        return count;
+    }
+}
