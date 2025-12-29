94aa32e81e350c6a7e9ca4fdf39d36150d1802c7
CHARUMATHIBALA <155135192+CHARUMATHIBALA@users.noreply.github.com>
2025-12-29 11:06:13 +0530

    duplicate removals

---
+++ easy/duplicate removals.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && c == stack.peek()) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
